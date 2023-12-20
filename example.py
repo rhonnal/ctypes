e6856b69c018cb4d113a90de817fde92e4f1fbc6
Jerrie <123565699+rhonnal@users.noreply.github.com>
2023-12-20 22:54:21 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,1 +1,12 @@
+# example.py
+import ctypes
 
+# Load the C++ library
+cpp_lib = ctypes.CDLL('./example.so')
+
+# Call a function from C++
+cpp_lib.hello_from_cpp()
+
+# Call a function with parameters
+result = cpp_lib.add_numbers(3, 4)
+print(f"Addition result: {result}")
